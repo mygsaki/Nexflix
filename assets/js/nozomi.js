22dfa76b2c3e820e989efab763aada803e49d17c
NOZOMI <nonnonnoninb@gmail.com>
2022-02-17 21:20:11 +0900

    ちょっとできましたー！！ コピペするだけなんで、さきさんの映画のとこも画像だけimgフォルダに入れてもらえば私やりまーす！！(o^^o)

---
+++ assets/js/nozomi.js
@@ -0,0 +1,8 @@
+$(function(){
+  $('img').on('mouseover',function(){
+      $(this).addClass('img-big');
+  })  
+  $('img').on('mouseleave',function(){
+      $(this).removeClass('img-big');
+  })  
+})
