1bafcb3578102bde5edaf8480ba7b5c8bd5f030a
NK3450B\user <shaxipingliang@gmail.com>
2022-02-18 16:01:54 +0900

    コミット

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,9 +9,6 @@ $(function(){
     $('.btn').on('click',function(){
         $('.btn').text('809-989');
         })
-        $('#look').on('click', function () {
-            $(this).siblings().slideToggle();
-        })
     
     
   
