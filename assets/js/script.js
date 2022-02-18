0843035c6afb0ab98cbf4e88f737bdc7b99894b7
NOZOMI <nonnonnoninb@gmail.com>
2022-02-18 15:33:26 +0900

    繋げた

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+$(function(){
+    $('img').on('mouseover',function(){
+        $(this).addClass('img-big');
+    })  
+    $('img').on('mouseleave',function(){
+        $(this).removeClass('img-big');
+    })
+
+    $('.btn').on('click',function(){
+        $('.btn').text('809-989');
+        })
+        $('#look').on('click', function () {
+            $(this).siblings().slideToggle();
+        })
+    
+    
+  
+  
+  
+  })
