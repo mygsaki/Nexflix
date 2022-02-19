0ccf60b2688b58409612ba8c3f547a556599f742
NOZOMI <nonnonnoninb@gmail.com>
2022-02-19 22:11:09 +0900

    私もできたー！！！！ でもheaderが隠れちゃう！！（ ;  ; ）なぜ（ ;  ; ） そして画像をホバーで拡大するとうまくいかないからとりあえず透けるようにした… 今ルイスさんに問い合わせ中笑

--- assets/js/nozomi.js
+++ assets/js/nozomi.js
@@ -1,12 +1,37 @@
-$(function(){
-  $('img').on('mouseover',function(){
-      $(this).addClass('img-big');
-  })  
-  $('img').on('mouseleave',function(){
-      $(this).removeClass('img-big');
-  })
+$(function () {
+  // $('img').on('mouseover',function(){
+  //     $(this).addClass('img-big');
+  // })  
+  // $('img').on('mouseleave',function(){
+  //     $(this).removeClass('img-big');
+  // })
+
+  $('.slider1').slick({
+    // autoplay:true,
+    // autoplaySpeed:5000,
+    dots: true,
+    slidesToShow: 4,
+    slidesToScroll: 1
+  });
   
-  
+  $('.slider2').slick({
+    // autoplay:true,
+    // autoplaySpeed:5000,
+    dots: true,
+    slidesToShow: 3,
+    slidesToScroll: 1
+  });
+
+  $('img').on('mouseover', function () {
+    $(this).css('opacity', 0.7);
+  });
+
+  $('img').on('mouseleave', function () {
+    $(this).css('opacity', 1);
+  });
+
+
+
 
 
 
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,10 @@
 $(function(){
-    $('img').on('mouseover',function(){
-        $(this).addClass('img-big');
-    })  
-    $('img').on('mouseleave',function(){
-        $(this).removeClass('img-big');
-    })
+    // $('img').on('mouseover',function(){
+    //     $(this).addClass('img-big');
+    // })  
+    // $('img').on('mouseleave',function(){
+    //     $(this).removeClass('img-big');
+    // })
 
     $('.btn').on('click',function(){
         $('.btn').text('809-989');
@@ -13,6 +13,31 @@ $(function(){
     $('#look').on('click',function(){
         $('#option').slideToggle();
     })
+
+
+    $('.slider1').slick({
+        // autoplay:true,
+        // autoplaySpeed:5000,
+        dots: true,
+        slidesToShow: 4,
+        slidesToScroll: 1
+      });
+      
+      $('.slider2').slick({
+        // autoplay:true,
+        // autoplaySpeed:5000,
+        dots: true,
+        slidesToShow: 3,
+        slidesToScroll: 1
+      });
+    
+      $('img').on('mouseover', function () {
+        $(this).css('opacity', 0.7);
+      });
+    
+      $('img').on('mouseleave', function () {
+        $(this).css('opacity', 1);
+      });
   
   
   
